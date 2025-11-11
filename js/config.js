d45ac2f87ee4ae709a0f831248994a00fa0bb184
manoj009876540987654 <144827758+manoj009876540987654@users.noreply.github.com>
2025-11-11 20:25:02 +0530

    Update Cognito configuration for new region

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-1_Z9f9WNUWJ',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '2ru1q0dgac13a4fh4mtiq7532j',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             'us-east-1'          // e.g. us-east-2
+        userPoolId:         'ap-south-1_FvL3BuV69',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '5t3979f9pn50497lmvqae5m98l',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'ap-south-1'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          'https://lngspfvcob.execute-api.us-east-1.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
